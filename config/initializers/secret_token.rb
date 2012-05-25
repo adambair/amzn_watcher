50d6b1a25753db7172eca8d02a5f887a6651f17f
David Wurtz <david@davidwurtz.com>
2012-05-25 12:36:16 -0600

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmznWatcher::Application.config.secret_token = '7a73deab252f9c86fd324269c35eacbaba790aea046166d34025746510c9d41e3fb6c7986c65d84911175d26fd91a91f8ea5b2c7cdc0a6b3f6a28e2b10f774c0'
